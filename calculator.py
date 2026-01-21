0c4b62d28dcbacad64f56dbe36ecc0296c5a2415
Abhinashrajput447 <2500030978@kluniversity.in>
2026-01-21 10:03:25 +0530

    name checking function code pushing on github

--- calculator.py
+++ calculator.py
@@ -1,52 +1,12 @@
-# # score = int(input("Enter your score:  "))
-
-# def grade():
-#   if score >= 100:
-#     print("Score doesn't exist for a single subject")
-#   elif score >= 90:
-#     print("Grade A")
-#   elif score >= 80:
-#     print("Grade B")
-#   elif score >= 70:
-#     print("Grade C")
-#   elif score >= 60:
-#     print("Grade D")
-#   else:
-#     print("Grade F")
-
-
-# num = int(input("enter your number: "))
-
-# def checkEvenOrOdd():
-#   if num%2 == 0 :
-#     # print("Even")
-#     return True
-#   else:
-#     # print("Which number you have entred that is odd")
-#     return False
-
-# grade()
-# checkEvenOrOdd()
-# # if
-
-
-def main():
-  x = int(input("what's x ? "))
-  if(is_even(x)):
-    print("Even")
+name = input("What's your name? ")
+def checkName(n):
+  if name == "Abhi" or name == "Abhinash" or name == "abhi":
+    print("Hii Abhinash")
+  elif name == "Vashu":
+    print("Hii Varsha")
+  elif name == "mom":
+    print("Good Morning MOM")
   else:
-    print("Odd")
+    print("Who are you")
 
-
-def is_even(n):
-  return n % 2 == 0
-  
-# x = int(input("what's x ? "))
-# def isEven(n):
-#   result = "even" if n%2==0 else "odd"
-#   print(result)
-# isEven(x)
-
-
-
-main()
+checkName(name)
