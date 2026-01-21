04f20887f6ee98510264e35c5688c0762d8399f3
Abhinashrajput447 <2500030978@kluniversity.in>
2026-01-21 09:44:30 +0530

    ood and even code pushing on github in python

--- calculator.py
+++ calculator.py
@@ -1,6 +1,47 @@
-# in this place of { => we keep ":" in python 
-x = int(input("what's x? "))
-y = int(input("what's y? "))
+# # score = int(input("Enter your score:  "))
 
-if x != y :
-  print("X != Y")
+# def grade():
+#   if score >= 100:
+#     print("Score doesn't exist for a single subject")
+#   elif score >= 90:
+#     print("Grade A")
+#   elif score >= 80:
+#     print("Grade B")
+#   elif score >= 70:
+#     print("Grade C")
+#   elif score >= 60:
+#     print("Grade D")
+#   else:
+#     print("Grade F")
+
+
+# num = int(input("enter your number: "))
+
+# def checkEvenOrOdd():
+#   if num%2 == 0 :
+#     # print("Even")
+#     return True
+#   else:
+#     # print("Which number you have entred that is odd")
+#     return False
+
+# grade()
+# checkEvenOrOdd()
+# # if
+
+
+def main():
+  x = int(input("what's x ? "))
+  if(is_even(x)):
+    print("Even")
+  else:
+    print("Odd")
+
+
+def is_even(n):
+  if n%2 == 0 :
+    return True
+  else:
+    return False
+  
+main()
