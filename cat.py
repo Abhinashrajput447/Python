2ea47f0d3dbd1a72e2c766bf6b7260e4cb9b566e
Abhinashrajput447 <2500030978@kluniversity.in>
2026-01-21 10:32:43 +0530

    while loop code pushing on github

---
+++ cat.py
@@ -0,0 +1,12 @@
+def cat():
+  print("meow")
+  print("meow")
+  print("meow")
+  print("meow")
+
+# cat()
+
+i = 1
+while i <= 10 :
+  print(i)
+  i += 1  # if we don;t do this this will upto infinte 
