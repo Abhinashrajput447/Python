cd9c3d1ce79857a8b49d1ee29e44bd0d298f5365
Abhinashrajput447 <2500030978@kluniversity.in>
2026-01-23 16:43:40 +0530

    some inbuilt function code pushing on github

--- loops.py
+++ loops.py
@@ -1,7 +1,21 @@
-students = ["Abhinash", "Varsha", "Ritik", "Ashutosh"]
+# students = ["Abhinash", "Varsha", "Ashutosh", "Ritik", "Bittu"]
 
-# print(students[0])/
+# for i in range(len(students)):
+#   print( students[i+1])
 
-for i in students:
-  print(i, end=" ")
+# size = len(students)
+# print(size)
 
+# for i, val in enumerate(['a','b','c']):
+#   print(i, val)
+
+# for a, b in zip([1,2, 3], [4,5]):
+#     print(a, b)
+
+
+# p = pow(2,5)
+# print(p)
+
+# print(abs(-1))
+
+print(isinstance(10, int))
