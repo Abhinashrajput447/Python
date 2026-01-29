9393a627e0c9f0a7dde9028f3c7913c3df2d748b
Abhinashrajput447 <2500030978@kluniversity.in>
2026-01-29 17:24:01 +0530

    some delete file again pushing on github

---
+++ Basics/mario.py
@@ -0,0 +1,8 @@
+n = int(input("enter a number:  "))
+def main():
+  print_squre(n)
+
+def print_squre(size):
+  for i in range(size):
+    print("#"*size)
+main()
--- Basics/practice.py
+++ Basics/practice.py
@@ -139,9 +139,13 @@ def trainge(n):
 def Z(n):
   for i in range(1, n+1):
     for j in range(1, n+1):
-      if i == 1 or i == n or i+j==n+1:
-        print('*', end=" ")
+      if i == 1 or i == n or i+j == n+1:
+        print('*', end="")
       else:
-        print(" ")
+        print(" ", end="")
     print()
+
+def printTraigleNum(n):
+
+
 Z(n)
